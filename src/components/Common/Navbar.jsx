01d2de30a2d693a2db9d87eb739baed1c345337d
kumarmadhan30242 <kumarmadhan30242@gmail.com>
2025-12-24 20:19:40 +0530

    new files added

--- src/components/Common/Navbar.jsx
+++ src/components/Common/Navbar.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import {HiOutlineUser,HiOutlineShoppingBag,HiBars3BottomRight} from 'react-icons/hi2'
+import { SearchBar } from './SearchBar'
 export const Navbar = () => {
   return (
     <>
-    
+    <nav className='container mx-auto flex items-center justify-between py-4 px-6'>
+        {/* left logo */}
+        <div>
+           <Link to='/' className='text-2xl font-medium'>Rabbit</Link>
+        </div>
+        {/* center Navigation Link */}
+        <div className='hidden md:flex space-x-6'>
+            <Link to="#" className='text-gray-500 hover:text-black text-sm font-medium uppercase'>
+            men</Link>
+             <Link to="#" className='text-gray-500 hover:text-black text-sm font-medium uppercase'>
+            women</Link>
+             <Link to="#" className='text-gray-500 hover:text-black text-sm font-medium uppercase'>
+            top wear</Link>
+             <Link to="#" className='text-gray-500 hover:text-black text-sm font-medium uppercase'>
+            bottom wear</Link>
+        </div>
+        {/* right icons */}
+        <div className='flex items-center space-x-4'>
+            <Link to='/profile' className='hover:text-black '>
+            <HiOutlineUser className='h-6 w-6 text-gray-500 hover:text-black'/>
+            </Link>
+            <button className='relative hover:text-black'>
+                <HiOutlineShoppingBag className='h-6 w-6 text-gray-500 hover:text-black'/>
+                <span className='absolute -top-1 bg-rabit-red text-white text-sm rounded-full px-2 py-0.5'>4</span>
+            </button>
+            {/* search */}
+            <SearchBar/>
+            <button className='md:hidden'>
+                <HiBars3BottomRight className='h-6 w-6 text-gray-500 hover:text-black'/>
+            </button>
+        </div>
+    </nav>
     </>
   )
 }
