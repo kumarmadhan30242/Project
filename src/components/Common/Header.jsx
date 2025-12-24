ee93141aee13f37dc443c1b68a183913141a4c33
kumarmadhan30242 <kumarmadhan30242@gmail.com>
2025-12-24 15:07:52 +0530

    new added files

--- src/components/Common/Header.jsx
+++ src/components/Common/Header.jsx
@@ -1,12 +1,14 @@
 import Topbar from "../Layouts/Topbar"
+import { Navbar } from "./Navbar"
 
 export const Header = () => {
   return (
-    <>
+    <header>
     {/* Topbar */}
     <Topbar/>
     {/* navbar */}
+    <Navbar/>
     {/* cart Drawer */}
-    </>
+    </header>
   )
 }
